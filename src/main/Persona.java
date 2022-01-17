187252f605bb106033545f19777a47492a4079ee
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-17 23:59:21 +0100

    Main y Persona

---
+++ src/main/Persona.java
@@ -0,0 +1,58 @@
+package main;
+
+public class Persona {
+	
+	//Definimos hombre por defecto con una CONSTANTE
+	private final char SEXO = 'H';
+	
+	//Atributos de clase
+	//Definimos los atributos y como no queremos que se acceda desde otra clase, los ponemos en private
+	//private bloquea el acceso de estas variables solo al ambito de la clase
+	private String nombre; //Podriamos dejarlo asi o en cadena vacia " "
+	private int edad;
+	private String DNI;
+	private char sexo;
+	private double peso;
+	private double altura;
+	
+	//Constructor 1 - Por defecto
+	//Los constructores de la clase deben ser del tipo public
+	public Persona () {
+		//Voy a que cuando instancie la clase tenga que pasarle el atributo nombre
+		this.nombre = "";
+		this.edad = 0;
+		this.DNI = "";
+		this.sexo = SEXO;
+		this.peso = 0.0;
+		this.altura = 0.0;		
+	}
+	//Constructor 2
+	public Persona (String nombreVal, int edadVal, char sexoVal) {
+		this.nombre = nombreVal;
+		this.edad = edadVal;
+		this.DNI = "12345678X";
+		this.sexo = sexoVal;
+		this.peso = 90.0;
+		this.altura = 173.0;
+	}
+	//Generar un constructor con boton derecho source generate constructor using fields
+	public Persona(String nombre, int edad, String dNI, char sexo, double peso, double altura) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		DNI = dNI;
+		this.sexo = sexo;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	//Getters y setters - source getters and setters. Nos permiten cambiar el valor de determinados atributos cuando ya se ha creado el objeto
+	
+	//Generar el metodo toString para pintarlo Source generate tostring
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + ", DNI=" + DNI + ", sexo=" + sexo + ", peso=" + peso
+				+ ", altura=" + altura + "]";
+	}
+	//Ya tenemos una clase que define atributos, 3 constructores y el metodo tostring
+	//Vamos a instanciarlo en mainAPP
+}
---
+++ src/main/mainAPP.java
@@ -0,0 +1,16 @@
+package main;
+
+public class mainAPP {
+
+	public static void main(String[] args) {
+		//Instancio la clase persona y creo el objeto persona
+		Persona p1 = new Persona (); //Llamada a la clase Persona dentro del mismo package (main)
+		//Vamos a crear 2 objetos mas
+		Persona p2 = new Persona ("Albert Notario", 33, 'H'); 
+		Persona p3 = new Persona ("Elena", 27, "12345678E", 'M', 55., 1.60); 
+		
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+	}
+}
